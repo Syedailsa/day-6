f8c27d7ea0c0aed8563699f538a60ba2b8840eea
Ilsa. <ilsasyeda@gmail.com>
2024-03-13 04:11:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var guest = [
+    "urooj",
+    "umair",
+    "ali",
+    "amal",
+];
+guest.push("bilal"); //at the end position
+guest.unshift("miral", "sundas"); //at the beggening position
+guest.splice(guest.length / 2, 0, "alla dawas"); //somewhere in middle (position , del count, string)
+console.log(guest);
